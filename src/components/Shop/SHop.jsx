dab74e366cfd5e7b3e691e88b5b9eac2fbe29ac6
mahmudfoysal280@gmail.com <mahmudfoysal280@gmail.com>
2023-03-28 21:27:39 +0600

    added product

--- src/components/Shop/SHop.jsx
+++ src/components/Shop/SHop.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 const SHop = () => {
   const [products, setProducts] = useState([]);
+  console.log(products);
 
   useEffect(() => {
     fetch("products.json")
@@ -13,7 +15,9 @@ const SHop = () => {
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h2>products coming here: {products.length}</h2>
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
